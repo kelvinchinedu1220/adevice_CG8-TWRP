f1fe349b0865440bfb0bfc9d823b655547f05580
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-03-24 22:41:04 +0100

    Update and rename omni_TECNO-CG8.mk to twrp_CG8.mk

---
+++ twrp_CG8.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit from CG8 device
+$(call inherit-product, device/tecno/CG8/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := CG8
+PRODUCT_NAME := twrp_CG8
+PRODUCT_BRAND := Tecno
+PRODUCT_MODEL := Camon 17 Pro
+PRODUCT_MANUFACTURER := Tecno
+PRODUCT_RELEASE_NAME := Tecno Camon 17 Pro
